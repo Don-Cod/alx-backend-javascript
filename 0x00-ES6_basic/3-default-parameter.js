08feb3589e7e7b2ab51fc1402f678f1db89d7f0c
Don-Cod <masyome@gmail.com>
2023-12-09 12:35:31 -0800

    ES6_promise

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,10 @@
+// Const getSumOfHoods = (initialNumber, expansion1989 = 89, expansion2019 = 19) => {
+//     Return initialNumber + expansion1989 + expansion2019;
+// }
+
+// Export default getSumOfHoods;
+
+
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
